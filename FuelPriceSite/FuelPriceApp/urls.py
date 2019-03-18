17a306abede54e256ab16d4e00e1be99a5b7e9b9
eljo <eljo.go2@gmail.com>
2019-03-18 17:07:22 -0500

    external CSS loading as Static files

--- FuelPriceSite/FuelPriceApp/urls.py
+++ FuelPriceSite/FuelPriceApp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('client_profile.html',ClientProfileView.as_view(),name = 'clientprofile'),
     path('get_quote.html',GetQuoteView.as_view(),name = 'getquote'),
     path('login.html',LoginView.as_view(), name = 'login'),
-    path('signup.html',LoginView.as_view(),name = 'signup'),
+    path('signup.html',SignupView.as_view(),name = 'signup'),
     path('quote_history.html',QuoteHistoryView.as_view(), name = 'quotehistory'),
 
 ]
--- FuelPriceSite/FuelPriceSite/settings.py
+++ FuelPriceSite/FuelPriceSite/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'CDT'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "FuelPriceApp/static")
+]
